42ac5081d2c023fcfbd18437d4e5b95b3334405d
tintin <tintin@brotherhood-Mac.local>
2017-01-21 14:17:02 -0800

    click to get the planet object

--- Assets/scripts/astronaut_controls.cs
+++ Assets/scripts/astronaut_controls.cs
@@ -11,10 +11,33 @@ public class astronaut_controls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
 		if (Input.GetMouseButton (0)) {
 			Debug.Log ("Pressed the left mouse button!");
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit, 100)) {
+				Debug.Log (hit.transform.gameObject.name);
+			}
+
 		}
 			
+	}
+
+
+	private Transform GetNearestPlanet(Transform[] planets){
+		Transform tMin = null;
+		float minDist = Mathf.Infinity;
+		Vector3 currentPos = transform.position;
+
+		foreach (Transform t in planets) {
+			//distance between current planet and astronaut
+			float dist = Vector3.Distance (t.position, currentPos);
+			if (dist < minDist) {
+				tMin = t; //new nearest planet
+				minDist = dist; //new nearest distance
+			}
+		}
+		return tMin; //return nearest planet
 	}
 }
