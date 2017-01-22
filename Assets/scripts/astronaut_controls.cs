4b1b45c3bdf286b719f84b9d407a112c0f83abfc
tintin <tintin@brotherhood-Mac.local>
2017-01-21 16:24:15 -0800

    not traversing to planet

--- Assets/scripts/astronaut_controls.cs
+++ Assets/scripts/astronaut_controls.cs
@@ -3,26 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class astronaut_controls : MonoBehaviour {
+	public float speed;
+	private bool _isJumping;
+	private Transform target; //next planet to jump to
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButton (0)) {
 			Debug.Log ("Pressed the left mouse button!");
-			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit hit;
-
-			if (Physics.Raycast (ray, out hit, 100)) {
-				Debug.Log (hit.transform.gameObject.name);
-			}
-
+			Vector3 mousePos = Input.mousePosition;
+			target = GetPlanetClicked (mousePos);
+			_isJumping = true;
 		}
-			
+
+		if (_isJumping) {
+			Debug.Log ("Jumping");
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+			Debug.Log ("Transform.position:" + transform.position.x + "," + transform.position.y);
+			Debug.Log ("TARGET.position:" + target.position.x + "," + target.position.y);
+		}
+
 	}
+
+	private Transform GetPlanetClicked(Vector3 mousePos){
+		Ray ray = Camera.main.ScreenPointToRay (mousePos);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 100)) {
+			Debug.Log (hit.transform.gameObject.name);
+			return hit.transform;
+		}
+		return null;
+	}
+
+
 
 
 	private Transform GetNearestPlanet(Transform[] planets){
