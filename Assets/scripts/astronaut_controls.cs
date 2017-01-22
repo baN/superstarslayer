b2e1762542df48c00c2135189bc5d5975d695602
tintin <tintin@brotherhood-Mac.local>
2017-01-22 13:59:29 -0800

    successful hit, lose all pieces of swords, disable sprite renderer, return back to initial position of astronaut

--- Assets/scripts/astronaut_controls.cs
+++ Assets/scripts/astronaut_controls.cs
@@ -3,46 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class astronaut_controls : MonoBehaviour {
+	public bool _hasBladeTop;
+	public bool _hasBladeLow;
+	public bool _hasHilt;
 	private bool _isJumping;
-	private bool _isDarkside;
+	private bool died = false;
 	private Transform target; //next planet to jump to
-	private Transform darkside;
+	private Transform darkside; //should always be going to the darkside unless jumping.
+	private Vector3 initialPosition; 
+
 
 	// Use this for initialization
 	void Start () {
-
+		initialPosition = transform.position;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		if (died) {
+			ResetSwordCollection ();
+			Invisible (true);
+			ResetPositionToInitial ();
+			died = false;
+			_isJumping = false;
+			target = null;
+			darkside = null;
+			MoveTo (transform.position, initialPosition);
+
+		}
 		if (Input.GetMouseButton (0)) {
 			Vector2 mousePos = Input.mousePosition;
 			target = GetPlanetClicked (mousePos);
 			_isJumping = true;
 		}
 		if (_isJumping && target) {
+			//planet is selected, start intial jump towards that planet
 			darkside = GetDarkside (target);
-			transform.position = Vector2.MoveTowards (transform.position, target.position, .10f);
+			MoveTo(transform.position, target.position);
 		}
-		if (_isDarkside && darkside) {
-			//go to the darkside
-			transform.position = Vector2.MoveTowards (transform.position, darkside.position, .10f);
+		if (darkside) {
+			//go to the darkside if planet parent is selected.
+			MoveTo(transform.position, darkside.position);
 		}
 
 
 	}
 
-	//when astronaut lands on object (planet)
+	//when astronaut hits something 2D.
 	void OnCollisionEnter2D (Collision2D col)
 	{
-		if (col.gameObject.name == "planet_darkside") {
-			_isDarkside = false;
-			Debug.Log ("hit the darkside of the planet");
-		} else {
+		if (col.gameObject.name == "UVBullet(Clone)") {
+			Debug.Log ("Astronaut was hit by a UVWave");
+			died = true;
+		}
+		else {
 			_isJumping = false; //stop going to the planet
-			_isDarkside = true; //start moving to the darkside
 			Debug.Log ("Stopped jumping");
 		}
+	}
+
+	public void MoveTo(Vector3 source, Vector3 destination){
+		transform.position = Vector2.MoveTowards (source, destination, .10f);
+	}
+
+	public void ResetSwordCollection(){
+		_hasBladeLow = false;
+		_hasBladeTop = false;
+		_hasHilt = false;
+	}
+
+	public void ResetPositionToInitial(){
+		transform.position = initialPosition;
+	}
+
+	public void Invisible(bool invis){
+		SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer> ();
+		if (sr != null)
+			sr.enabled = invis;
 	}
 
 	private Transform GetDarkside(Transform planet){
